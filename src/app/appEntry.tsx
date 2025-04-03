ae93b48c3de0c1806561fdd97d8fe1095447c489
Zamchik <zamchik28@mail.ru>
2025-04-03 03:03:28 +0300

    add newsPage and React Router

--- src/app/appEntry.tsx
+++ src/app/appEntry.tsx
@@ -2,15 +2,16 @@ import { ThemeProvider } from "@/app/providers/ThemeProvider";
 import React from "react";
 import ReactDOM from 'react-dom/client';
 import { Provider } from "react-redux";
-import BaseLayout from "./layouts/BaseLayout";
 import { store } from "./appStore";
 import "@/shared/index.css"
+import { RouterProvider } from "react-router-dom";
+import { appRouter } from "./appRouter";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <ThemeProvider>
             <Provider store={store}>
-                <BaseLayout />
+                <RouterProvider router={appRouter} />
             </Provider>
         </ThemeProvider>
     </React.StrictMode>,
--- src/widgets/news/ui/NewsList/NewsList.tsx
+++ src/widgets/news/ui/NewsList/NewsList.tsx
@@ -2,18 +2,20 @@ import { INews } from "@/entities/news";
 import withSkeleton from "@/shared/hocs/withSkeleton";
 import styles from "./styles.module.css";
 import NewsCard from "@/entities/news/ui/NewsCard/NewsCard";
+import { ReactNode } from "react";
 
 interface Props {
   news?: INews[];
   type?: "banner" | "item";
   direction?: "row" | "column";
+  viewNewSlot?: (news: INews) => ReactNode
 }
 
-const NewsList = ({ news, type = "item" }: Props) => {
+const NewsList = ({ news, type = "item", viewNewSlot }: Props) => {
   return (
     <ul className={`${type === "item" ? styles.items : styles.banners}`}>
       {news?.map((item) => {
-        return <NewsCard key={item.id} item={item} type={type}/>;
+        return <NewsCard key={item.id} item={item} type={type} viewNewSlot={viewNewSlot}/>;
       })}
     </ul>
   );
